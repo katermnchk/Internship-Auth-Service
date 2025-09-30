0eb8715ad336b23cfe3ec45c99fe025cab07114e
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-30 22:22:18 +0300

    init commit

---
+++ src/main/java/com/innowise/authenticationservice/InnowiseAuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseAuthentificationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InnowiseAuthentificationServiceApplication.class, args);
+  }
+
+}
